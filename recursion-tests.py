83885e6e269c21fd126f6c5fa97c2bec511ee2bf
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-01-20 22:31:17 +0300

    intro to recursion and example

---
+++ recursion-tests.py
@@ -0,0 +1,15 @@
+# Recursion - A function that calls itself
+# Every recursive function has two parts
+# 1. Base Case - (When to stop) -> Eg. if this is a file, don't go deeper
+# 2. Recursive Case - (Call itself) -> eg. if this is a folder, scan it
+
+# Example 1 (Recursion with  numbers)
+
+def countdown(n):
+    if n == 0: # Base Case
+        return
+    
+    print(n)
+    countdown(n-1) # Recursive Call
+    
+countdown(10)
