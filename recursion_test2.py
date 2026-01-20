9b031642bc69f20b9fbbe2fe83696805efb53814
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-01-21 01:57:31 +0300

    add extension checking functionality

--- recursion_test2.py
+++ recursion_test2.py
@@ -3,9 +3,10 @@ from pathlib import Path
 file_count = 0
 folder_count = 0
 total_size = 0
+files_by_extension = {}
 
 def scan(path):
-    global file_count, folder_count, total_size
+    global file_count, folder_count, total_size, files_by_extension
     
 # since path.iterdir talks directly to the OS, if the folder is protected,
 # is a system directory or is locked/you don't have permissions the OS throws an error
@@ -18,10 +19,21 @@ def scan(path):
     for item in items:
         if item.is_file():
             file_count += 1
+            
+            # file size functionality
             try:
                 total_size += item.stat().st_size
             except OSError: # OSError used here because file may disappear, may be locked or OS refuses metadata access during scanning
-                pass  
+                pass 
+            
+            # file extension functionality
+            extension = item.suffix.lower()
+            if extension == "":
+                extension = "NO_EXTENSION"
+                
+            files_by_extension[extension] = files_by_extension.get(extension, 0) + 1
+            
+            
         elif item.is_dir():
             folder_count += 1
             scan(item)
@@ -32,3 +44,6 @@ scan(start_path)
 print("Files:", file_count)
 print("Folders:", folder_count)
 print("Total size (bytes):", total_size)
+print("\nFiles by extension:")
+for extension, count in files_by_extension.items():
+    print(extension, "->", count)
